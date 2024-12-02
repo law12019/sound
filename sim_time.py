bcb7107e7fe5f3478211527daa359b6af02dfcfa
Charles law <charles.law@kitware.com>
2024-12-02 17:54:34 -0500

    multiple sources

--- sim_time.py
+++ sim_time.py
@@ -22,15 +22,34 @@ two_pi = 2.0 * math.pi
 print("frequency  ", frequency)
 
 
-source1 = simple_sphere(pos=vector(-2,-3,0), color=color.red, radius=0.25)
+
+
+sources = []
+
+
+
+
+
+for index in range(13):
+    source = simple_sphere(pos=vector(-3+(index*0.5),-3-3,0), color=color.red, radius=0.25)
+    # Phase and amplitude.
+    source.amplitude = amplitude
+    source.frequency = frequency
+
+
+source = simple_sphere(pos=vector(-2,-3,0), color=color.red, radius=0.25)
 # Phase and amplitude.
-source1.amplitude = amplitude
-source1.phase = phase
-source1.frequency = frequency + 10
-source2 = simple_sphere(pos=vector(2,-3,0), color=color.red, radius=0.25)
-source2.amplitude = amplitude
-source2.phase = 0  #math.pi  # phase
-source2.frequency = frequency
+source.amplitude = amplitude
+source.phase = phase
+source.frequency = frequency + 10
+sources.append(source)
+
+
+source = simple_sphere(pos=vector(2,-3,0), color=color.red, radius=0.25)
+source.amplitude = amplitude
+source.phase = 0  #math.pi  # phase
+source.frequency = frequency
+sources.append(source)
 
 
 
@@ -55,35 +74,27 @@ t = 0
 
 for time_counter in range(1000):
     for vert in vertices:
-        # phase and amplitude change.
-        dist_1 = mag(vert.pos - source1.pos)
-        dist_1 = max(dist_1, source1.radius)
-        amplitude_1  = source1.amplitude / (dist_1**2)
-        phase_1 = source1.phase 
-        phase_1 += two_pi * source1.frequency * t
-        phase_1 += two_pi * dist_1 * source1.frequency / speed_of_sound
-        # phase and amplitude change.
-        dist_2 = mag(vert.pos - source2.pos)
-        dist_2 = max(dist_2, source1.radius)
-        amplitude_2  = source2.amplitude / (dist_2**2)
-        phase_2 = source2.phase 
-        phase_2 += two_pi * source2.frequency * t
-        phase_2 += two_pi * dist_2 * source2.frequency / speed_of_sound
-        # Add up the vectors.
-        vx_1 = amplitude_1 * cos(phase_1)
-        vy_1 = amplitude_1 * sin(phase_1)
-        vx_2 = amplitude_2 * cos(phase_2)
-        vy_2 = amplitude_2 * sin(phase_2)
-        vx = vx_1 + vx_2
-        vy = vy_1 + vy_2
-        vmag = math.sqrt(vx**2 + vy**2)
+        vx_total = 0
+        vy_total = 0
+        for source in sources:
+            # phase and amplitude change.
+            dist = mag(vert.pos - source.pos)
+            dist = max(dist, source.radius)
+            amplitude  = source.amplitude / (dist**2)
+            phase = source.phase 
+            phase += two_pi * source.frequency * t
+            phase += two_pi * dist * source.frequency / speed_of_sound
+            # Add up the vectors.
+            vx = amplitude * cos(phase)
+            vy = amplitude * sin(phase)
+            vx_total += vx
+            vy_total += vy
+        vmag = math.sqrt(vx_total**2 + vy_total**2)
         
         # Plot decibels.
         decibels = 10 * math.log10(vmag / I0)
         decibel_max = max(decibel_max, decibels)
         decibel_min = min(decibel_min, decibels)
-        #print(x, " ", y, " : dist ", dist_1, "  mag:", decibels)
-        #print(x, " ", y, " : dist ", dist_1, " ", dist_2, "  amp: ", amplitude_1, "  ", amplitude_2)
 
         # Scale the color range to see interference pattern.
         green = (decibels - 32.0) / 20.0
