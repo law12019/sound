eee3e916c44bf2c2b3241b814b2a14f7d16d5c0f
Charles law <charles.law@kitware.com>
2024-12-03 20:06:10 -0500

    video

--- sim_phase_array.py
+++ sim_phase_array.py
@@ -7,6 +7,8 @@ import time
 
 # Phase array of sources for directed beam
 
+# ffmpeg -framerate 30 -pattern_type glob -i '*.png' -c:v libx264 -pix_fmt yuv420p out.mp4
+
 
 #I0 = 1.0e-12          # W/m^2
 I0 = 1.0e-6          # W/m^2
@@ -41,6 +43,8 @@ db_min = 48
 db_max = 54
 
 
+rate(2)
+
 vertices = []
 dx = 0.1
 dy = 0.1
@@ -50,21 +54,39 @@ xmax = 3
 decibel_max = 0
 decibel_min = 351
 
+frame_number = 240
+number_of_frames = 120
+
 for y in np.arange(0, 12, dy):
-    for x in np.arange(-xmax-6, xmax+6, dx):
+    for x in np.arange(-xmax-7, xmax+7, dx):
         vert = box(pos=vector(x,y,0), size=vector(dx,dy,0.01))
         vert.shininess = 0.0
         vertices.append(vert)
 
 scene.autoscale = False
 scene.camera.axis = vector(0, 0, -1)
-scene.camera.pos = vector(0, 6, 10)
+scene.camera.pos = vector(0, 5.1, 10)
 scene.camera.up = vector(0, 1, 0)
+
+scene.camera.canvas.width = 1280
+scene.camera.canvas.height = 800
+
+
+my_text = text(text='Clara Law, Vicky Yan - Exploring Wave Dynamics: A Computational Model for Sound Wave Interference - Matter & Interactions I (2024)', pos=vector(0, -0.55, 0), align='center', color=color.white)
+my_text.height = 0.4
+my_text.length = 18.0
+my_text.depth = 0
+
+title_text = text(text='Phase Array Directional Speaker - 2D simulation - 48 to 54 decibels ', pos=vector(0, 10.25, 0.2), align='center', color=color.white)
+title_text.height = 0.4
+title_text.length = 16.0
+title_text.depth = 0
 
 
 
 # animate the direction
-for dir_x in range(60):
+for frame_idx in range(number_of_frames):
+    dir_x = frame_idx * 60 / number_of_frames
     green_min = 10000
     green_max = 0
     # change the phases.  Move the beam back and forth
@@ -115,7 +137,9 @@ for dir_x in range(60):
         vert.color = vector(0, green, green)
 
     #pdb.set_trace()
+    scene.capture(f"frame{frame_number:05}")
+    print(frame_idx, "  ", frame_number)
+    frame_number += 1
     print(green_min, "  ", green_max)
-    sleep(1)
 
 
